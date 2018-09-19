69d296ca465d9ad4517ff21da030e3cf3cc1adba
ming.li@iqunxing.com <ming.li@iqunxing.com>
2018-09-19 18:05:28 +0800

    redis

--- src/test/java/com/example/testController/TestSimpleRedis.java
+++ src/test/java/com/example/testController/TestSimpleRedis.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 public class TestSimpleRedis {
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
