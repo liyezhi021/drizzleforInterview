c296b16f82917e31b9cb494c394f92517e9e9a10
paynelove <835707341@qq.com>
2018-12-06 23:40:03 +0800

    leetcode solutions

---
+++ src/main/java/com/example/leetcode/Example/Solution.java
@@ -0,0 +1,29 @@
+package com.example.leetcode.Example;
+
+import java.math.BigDecimal;
+
+/**You are given two non-empty linked lists representing two non-negative integers. The digits are stored in
+ * reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ Example:
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+ Explanation: 342 + 465 = 807.
+
+ 这道题不用大数 int存不下 第一次RuntimeError 第二次AC
+ #############################ACCEPTED!###############################
+ **/
+
+public class Solution {
+
+    public static int addTwoNumbers(int i){
+
+        return 0;
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println();
+    }
+}
