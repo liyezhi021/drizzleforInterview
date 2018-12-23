c15031bc95c218c9537430bf286ca5d0f832dce9
paynelove <835707341@qq.com>
2018-12-24 06:16:09 +0800

    dubbo demo

---
+++ src/main/java/com/example/java/effectiveJava/TestBigInter.java
@@ -0,0 +1,15 @@
+package com.example.java.effectiveJava;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Random;
+
+public class TestBigInter {
+
+    public static void main(String[] args){
+        Random rd = new Random();
+        Collection
+        BigInteger bigInteger = BigInteger.probablePrime(1, 1000, rd);
+    }
+
+}
