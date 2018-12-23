4174c141e6ea075f277c00ba268f0eb08760ae42
paynelove <835707341@qq.com>
2018-12-24 06:17:55 +0800

    dubbo demo

---
+++ dubbo-client/src/main/java/com/example/dubboclient/DubboClientApplication.java
@@ -0,0 +1,20 @@
+package com.example.dubboclient;
+
+import com.example.dubboclient.dubbo.CityDubboConsumerService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DubboClientApplication {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        ConfigurableApplicationContext run = SpringApplication.run(DubboClientApplication.class, args);
+        CityDubboConsumerService cityService = run.getBean(CityDubboConsumerService.class);
+        cityService.printCity();
+    }
+
+}
+
