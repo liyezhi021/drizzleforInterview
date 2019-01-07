01a2d893c1f7596be0243ad90b41a99b25cb42d5
ming.li <ming.li@iqunxing.com>
2019-01-07 18:23:39 +0800

    rest

---
+++ src/main/java/com/example/algorithm/ercha/Node.java
@@ -0,0 +1,19 @@
+package com.example.algorithm.ercha;
+
+public class Node {
+
+    int key;
+    int value;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(int key,int value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public void displayNode(){
+
+    }
+
+}
